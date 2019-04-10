e1c13d6e878ca06017e9075e6637c6add4312619
larslasson <44834025+larslasson@users.noreply.github.com>
2019-04-10 12:28:22 +0200

    Maps and accordion

--- Google maps/main.js
+++ Google maps/main.js
@@ -1,23 +1,18 @@
-/*function goToCanada() {
-    let location = "Canada";
-    document.getElementById('gmap_canvas').src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18876468.200307723!2d-113.72221585646199!3d54.72270517403909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b0d03d337cc6ad9%3A0x9968b72aa2438fa5!2sCanada!5e0!3m2!1sno!2sno!4v1554288104631!5m2!1sno!2sno";
-}*/
-
-function goToZA() {
-    let location = "Republic of South Africa";
-    document.getElementById('gmap_canvas').src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13503651.299495872!2d18.256990247617672!3d-34.27941023734731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1c34a689d9ee1251%3A0xe85d630c1fa4e8a0!2sS%C3%B8r-Afrika!5e0!3m2!1sno!2sno!4v1554288401756!5m2!1sno!2sno";
-}
-
 function noroffBergen() {
-    let location = "Noroff Bergen";
-    document.getElementById('gmap_canvas').src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1973.1205441120076!2d5.3456640161866815!3d60.360647534946665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x463cfc01b28180bd%3A0x74b5075aad390869!2sNoroff+Bergen!5e0!3m2!1sno!2sno!4v1554288559900!5m2!1sno!2sno";
+    document.getElementById('gmap_canvas').src = "https://www.google.com/maps/d/embed?mid=12VIqI1hsD2hUFlg-1PhM2xHNGmxGAde-&hl=en";
 }
 
 
 function noroffOslo() {
-    let location = "Noroff Bergen";
-    document.getElementById('gmap_canvas').src = "  https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.1376914804757!2d10.753907416173204!3d59.929857369768925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e625bebdd55%3A0x87598940160b87fa!2sNoroff+Oslo!5e0!3m2!1sno!2sno!4v1554288634869!5m2!1sno!2sno";
+    document.getElementById('gmap_canvas').src = "https://www.google.com/maps/d/embed?mid=1KOdX_84cgwmBo6oDS6tzFmfgUQ15S3ib&hl=en";
 } 
+
+
+function noroffStavanger() {
+    document.getElementById('gmap_canvas').src = "https://www.google.com/maps/d/embed?mid=1Afy65N70tkZUhRFXYzW1WRi58k4J1BxD&hl=en";
+} 
+
+    
     
     
     
---
+++ accordion/main.js
@@ -0,0 +1,34 @@
+
+function toggleDisplay1() {
+    var y = document.getElementById("accordionTrigger1");
+    y.style.display ="block";
+    var x = document.getElementById("accordionContent1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    
+  }
+
+  function toggleDisplay2() {
+    var y = document.getElementById("accordionTrigger2");
+    y.style.display ="block";
+    var x = document.getElementById("accordionContent2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleDisplay3() {
+    var y = document.getElementById("accordionTrigger3");
+    y.style.display ="block";
+    var x = document.getElementById("accordionContent3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
